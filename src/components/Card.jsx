6278253228066cdf93e7073455a3620ee5eda556
Angel Garcia <angeliux.jossue@gmail.com>
2023-02-01 21:30:03 -0600

    card info glassmorphism effect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,38 @@
-function Card () {
+import '../styles/App.css'
+import Weatherinfo from './Weatherinfo'
+
+function Card() {
     return (
-        <h2>Hello</h2>
+        <div className="card">
+            <div className="card__search">
+                <input className="card__search-input" type="text" placeholder="Buscar Ciudad..." />
+                {/* <i className="uil uil-search card__search-icon"></i> */}
+            </div>
+            <div className="date">
+                <p className='date__local'>Miercoles, 01 FEB 2023 | Hora local: 02:50 AM</p>
+            </div>
+            <div className='card__box'>
+                <div className="card__content">
+                    <div className="card__weather">
+                        <h2 className='card__weather-city'> CDMX</h2>
+                        <p className="card__weather-temp">22°C </p>
+                        <p className="card__weather-main">Nublado</p>
+                    </div>
+                    
+                    <div className="card__info">
+                        <Weatherinfo title="Sensacion Termica" information="9°C"/>
+                        <Weatherinfo title="Humedad" information="50%"/>
+                        <Weatherinfo title="Precipitaciones" information="13%"/>
+                        <Weatherinfo title="Viento" information="13 Km/h"/>
+                    </div>
+                    <div className="general__info">
+                        <h3 className='info'>Amanecer: 6:15 AM | Atardecer: 7:28 PM | 9°C / 28°C</h3>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
     )
 }
+
+export default Card
