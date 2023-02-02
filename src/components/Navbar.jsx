05d2f8cea7a680dc05564e287d4d0571d3c6d8fb
Angel Garcia <angeliux.jossue@gmail.com>
2023-02-01 21:29:17 -0600

    navbar adjust

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,10 @@ import '../styles/App.css'
 
 function Navbar() {
   return (
-    <navbar className='navbar'>
-      <h1 className='navbar__title'>Weather Up</h1>
+    <nav className='navbar'>
+      <h1 className='navbar__title'>WeatherUp</h1>
       <p className='navbar__description'>Clima de hoy</p>
-    </navbar>
+    </nav>
   )
 }
 
